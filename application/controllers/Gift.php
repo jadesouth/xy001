1bed0fdfdf145532e5badf8d949ff664305ae6a5
haokaiyang <haokaiyang@anhao.cn>
2017-04-10 23:55:52 +0800

    添加订购礼物页面

---
+++ application/controllers/Gift.php
@@ -0,0 +1,13 @@
+<?php
+
+class Gift extends Home_Controller
+{
+    /**
+     * index 订购礼物
+     *
+     */
+    public function index()
+    {
+        $this->load_view();
+    }
+}
--- application/views/home/home/index.php
+++ application/views/home/home/index.php
@@ -1,3 +1,36 @@
+<div class="modal fade" id="loginmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title text-center" id="exampleModalLabel">账号登录</h4>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <!--<label for="recipient-name" class="control-label">邮箱:</label>-->
+                        <input type="email" class="form-control" id="recipient-user" placeholder="邮箱：" required>
+                    </div>
+                    <div class="form-group">
+                        <!--<label for="recipient-name" class="control-label">密码:</label>-->
+                        <input type="password" class="form-control" id="recipient-pwd" placeholder="密码：" required>
+                    </div>
+                </form>
+
+            </div>
+            <div class="modal-footer " style="text-align: center;">
+                <button type="button" id="loginuser" class="btn btn-primary" style="width:48%;">登录</button>
+                <button type="button" id="userregister" class="btn btn-primary" style="width:48%;">注册</button>
+                <div class="text-right">
+                    <a href="password.html" style="font-size: 12px;text-align:right ">忘记密码？</a>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
 <div class="main-content">
     <div class="section banner">
         <img src="/resources/assets/images/banner.png" alt="banner"/>
