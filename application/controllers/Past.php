f1518d5c1a2e15ab5aaa09c19336ea96432967a5
haokaiyang <kaiyanghao@gmail.com>
2017-04-11 22:20:19 +0800

    往期回顾页面及方法

--- application/controllers/Past.php
+++ application/controllers/Past.php
@@ -4,10 +4,43 @@ class Past extends Home_Controller
 {
     /**
      * index 往期回顾
-     *
      */
     public function index()
     {
+        $this->load->model('box_model');
+        $theme_and_year = $this->box_model->readAllThemeAndYear();
+        $this->_viewVar['theme_and_year'] = $theme_and_year;
+        $this->load->helper('http');
+        $year = (int)$this->input->get('year');
+        $theme_id = (int)$this->input->get('theme_id');
+        $select_fields = 'id,theme_id,theme_name,year,month,cover_title,cover_subtitle,cover_image';
+        $box_list = $this->box_model->readBox($theme_id, $year, $select_fields, 0, 21);
+        $this->_viewVar['box_list'] = $box_list;
         $this->load_view();
+    }
+
+    /**
+     * ajax 获取盒子介绍
+     */
+    public function ajaxGetBoxIntroduction()
+    {
+        if ('post' == $this->input->method()) {
+            $this->load->helper('http');
+            $box_id = (int)$this->input->post('id');
+            if (empty($box_id)) {
+                http_ajax_response(1, '非法请求', []);
+                return;
+            }
+            $this->load->model('box_model');
+            $this->box_model->setSelectFields('id,theme_id,introduction_title,introduction_image');
+            $box_info = $this->box_model->find($box_id);
+            if (! empty($box_info)) {
+                http_ajax_response(0, '成功', $box_info);
+                return;
+            }
+            http_ajax_response(1, '请稍后再试试', []);
+            return;
+        }
+        http_ajax_response(1, '非法请求', []);
     }
 }
