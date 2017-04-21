473fff5a3096808f376127c743f3eceaabfafeb1
wangnan <jadesouth@aliyun.com>
2017-04-21 18:20:20 +0830

    Code Clean

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -11,6 +11,7 @@
  * @property Coupon_model                  $coupon_model
  * @property Order_model                   $order_model
  * @property Order_plan_model              $order_plan_model
+ * @property Show_wall_model               $show_wall_model
  * @property Theme_model                   $theme_model
  * @property User_model                    $user_model
  */
