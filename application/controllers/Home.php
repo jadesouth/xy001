da1cbc69b42f890d6dd8602881f82cc664c1b9aa
wangnan <wangnan@anhao.com>
2017-05-17 10:57:59 +0830

    首页图片墙

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,13 +4,26 @@ class Home extends Home_Controller
 {
     /**
      * index 网站首页
-     *
      */
     public function index()
     {
         $this->load->model('box_model');
         $box_list = $this->box_model->getHomePageBoxInfo();
         $this->_viewVar['box_list'] = $box_list;
+        // 获取图片墙
+        $this->load->model('show_wall_model');
+        $showWalls = $this->show_wall_model->setSelectFields('id,image,url,type')
+            ->read();
+        $videosWall = null;
+        $imagesWall = null;
+        if (! empty($showWalls)) {
+            foreach ($showWalls as $showWall) {
+                0 == $showWall['type'] && $videosWall[] = $showWall;
+                1 == $showWall['type'] && $imagesWall[] = $showWall;
+            }
+        }
+        $this->_viewVar['videosWall'] = $videosWall;
+        $this->_viewVar['imagesWall'] = $imagesWall;
         $this->load_view();
     }
 }
