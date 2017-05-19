ab8abccd40782855dbd94cf5815a7773e141ade3
haokaiyang <kaiyanghao@gmail.com>
2017-05-19 22:49:12 +0800

    投票

--- application/controllers/Vote.php
+++ application/controllers/Vote.php
@@ -2,13 +2,18 @@
 /**
  * Class Vote 投票控制器
  */
-class Vote extends Admin_Controller
+class Vote extends Home_Controller
 {
     /**
      * partake 投票
      */
     public function partake()
     {
+        $show_vote_status = $this->session->userdata('show_vote_status');
+        if (! $show_vote_status) {
+            http_ajax_response(1, '非法请求');
+            return;
+        }
         $this->load->helper('http');
         $vote_id = (int)$this->input->post('vote', 0);
         if (0 >= $vote_id) {
@@ -18,6 +23,7 @@ class Vote extends Admin_Controller
 
         $this->load->model('vote_model');
         if (true == $this->vote_model->plus1($vote_id)) {
+            $this->session->unset_userdata('show_vote_status');
             http_ajax_response(0, '投票成功');
         } else {
             http_ajax_response(2, '投票失败');
