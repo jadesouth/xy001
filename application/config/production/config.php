0633b9cf8aaff0927655587c0f9df910b18ee4d2
wangnanphp <wangnanphp@163.com>
2017-05-21 12:45:36 +0800

    product env

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -1,4 +1,4 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = '';
+$config['base_url'] = 'http://www.amazingfun.cn/';
--- index.php
+++ index.php
@@ -53,7 +53,8 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+//	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+define('ENVIRONMENT', 'iZ2ze0oopvm45d9ahsns19Z' == gethostname() ? 'production' : 'development');
 
 /*
  *---------------------------------------------------------------
